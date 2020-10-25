722e0e85e6ef4ff4e1b03751379751827f96e0e8
Andrew <44542704+Endermanch@users.noreply.github.com>
2020-10-25 19:29:10 +0300

    LIME_S

--- shared/rtl.h
+++ shared/rtl.h
@@ -35,101 +35,80 @@ typedef CONST UNICODE_STRING *PCUNICODE_STRING;
 #define UNICODE_STRING_MAX_BYTES ((USHORT)65534)
 #define UNICODE_STRING_MAX_CHARS ((USHORT)32767)
 
-#define RTL_CONSTANT_UNICODE_STRING(s) {(sizeof(s)-2)/sizeof(wchar_t), sizeof(s), s}
+#define RTL_CONSTANT_UNICODE_STRING(s) {(sizeof(s) - 2) / sizeof(wchar_t), sizeof(s), s}
 
 #define UPPER(c)		((c) >= 0x61 && (c) <= 0x7A ? (c) - 0x20 : (c))
 
-NTSYSAPI
-VOID
-RtlInitUnicodeString(
-	__out PUNICODE_STRING DestinationString,
-	__in PWSTR SourceString
+NTSYSAPI ULONG RtlStringLength(
+	__in PCWSTR String
 );
 
-NTSYSAPI
-ULONG
-RtlStringLength(
-	__in PCWSTR SourceString
+NTSYSAPI VOID RtlStringCopy(
+	__inout PWSTR DestinationString,
+	__in	PWSTR SourceString
 );
 
-NTSYSAPI
-USHORT
-RtlStringCompare(
+NTSYSAPI VOID RtlStringCopyLength(
+	__inout PWSTR DestinationString,
+	__in	PWSTR SourceString,
+	__in	ULONG Characters
+);
+
+NTSYSAPI LONG RtlStringCompare(
 	__in PCWSTR String1,
 	__in PCWSTR String2
 );
 
-NTSYSAPI
-VOID
-RtlStringCopy(
-	__out PWSTR DestinationString,
-	__in PWSTR SourceString
-);
-
-NTSYSAPI
-VOID
-RtlStringCopyLength(
-	__out PWSTR DestinationString,
-	__in PWSTR SourceString,
-	__in ULONG Characters
-);
-
-NTSYSAPI
-USHORT
-RtlStringCompareLength(
+NTSYSAPI LONG RtlStringCompareLength(
 	__in PCWSTR String1,
 	__in PCWSTR String2,
-	__in ULONG Characters
+	__in ULONG	Characters
 );
 
-NTSYSAPI
-NTSTATUS
-RtlUnicodeStringValidate(
-	__in PCUNICODE_STRING SourceString
+NTSYSAPI NTSTATUS RtlUnicodeStringValidate(
+	__in PUNICODE_STRING UnicodeString
 );
 
-NTSYSAPI
-NTSTATUS
-RtlUnicodeStringCopy(
-	__out PUNICODE_STRING DestinationString,
-	__in PCUNICODE_STRING SourceString
+NTSYSAPI VOID RtlInitUnicodeString(
+	__inout PUNICODE_STRING	StringInformation,
+	__in	PWSTR			String
 );
 
-NTSYSAPI
-ULONG
-RtlUnicodeStringCompare(
-	__in PUNICODE_STRING String1,
-	__in PUNICODE_STRING String2
+NTSYSAPI VOID RtlAllocateAndInitUnicodeString(
+	__inout PUNICODE_STRING	AllocatedString,
+	__in	PWSTR			SourceString
 );
 
-NTSYSAPI
-ULONG
-RtlUnicodeStringCompareLength(
-	__in PUNICODE_STRING String1,
-	__in PUNICODE_STRING String2,
-	__in ULONG Characters
+NTSYSAPI VOID RtlAllocateAndInitUnicodeStringEx(
+	__inout PUNICODE_STRING	*AllocatedString,
+	__in	PWSTR			SourceString
 );
 
-NTSYSAPI
-VOID
-RtlAllocateAndInitUnicodeStringEx(
-	__out PUNICODE_STRING* AllocatedString,
-	__in PWSTR SourceString
-);
-
-NTSYSAPI
-VOID
-RtlAllocateAndInitUnicodeString(
-	__out PUNICODE_STRING AllocatedString,
-	__in PWSTR SourceString
-);
-
-NTSYSAPI
-VOID
-RtlFreeUnicodeString(
+NTSYSAPI VOID RtlFreeUnicodeString(
 	__in PUNICODE_STRING AllocatedString
 );
 
+NTSYSAPI NTSTATUS RtlUnicodeStringCopy(
+	__inout PUNICODE_STRING	DestinationUnicodeString,
+	__in	PUNICODE_STRING	SourceUnicodeString
+);
+
+NTSYSAPI NTSTATUS RtlUnicodeStringCopyLength(
+	__inout PUNICODE_STRING	DestinationUnicodeString,
+	__in	PUNICODE_STRING	SourceUnicodeString,
+	__in	ULONG			Characters
+);
+
+NTSYSAPI ULONG RtlUnicodeStringCompare(
+	__in PUNICODE_STRING UnicodeString1,
+	__in PUNICODE_STRING UnicodeString2
+);
+
+NTSYSAPI ULONG RtlUnicodeStringCompareLength(
+	__in PUNICODE_STRING	UnicodeString1,
+	__in PUNICODE_STRING	UnicodeString2,
+	__in ULONG				Characters
+);
 
 NTSYSAPI void* _memset( void* m1, unsigned char v, __int64 n );
 NTSYSAPI void* _memcpy( void* dst, void* src, int n );
