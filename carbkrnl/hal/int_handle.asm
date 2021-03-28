1773ccab9c5c9498315c3a6439afe2f547440865
irql0 <38012156+irql0@users.noreply.github.com>
2021-03-28 15:38:24 +0100

    r

--- carbkrnl/hal/int_handle.asm
+++ carbkrnl/hal/int_handle.asm
@@ -39,18 +39,18 @@ EXTERN      KiIpiCall
     push    rax
     push    rcx
     push    rdx
-	push    rbx
-	push    rbp
-	push    rsi
-	push    rdi
-	push    r8
-	push    r9
-	push    r10
-	push    r11
-	push    r12
-	push    r13
-	push    r14
-	push    r15
+    push    rbx
+    push    rbp
+    push    rsi
+    push    rdi
+    push    r8
+    push    r9
+    push    r10
+    push    r11
+    push    r12
+    push    r13
+    push    r14
+    push    r15
 
     mov     rax, ds
     push    rax
@@ -111,20 +111,20 @@ EXTERN      KiIpiCall
     mov     ds, rax
 
     pop     r15
-	pop     r14
-	pop     r13
-	pop     r12
-	pop     r11
-	pop     r10
-	pop     r9
-	pop     r8
-	pop     rdi
-	pop     rsi
-	pop     rbp
-	pop     rbx
-	pop     rdx
-	pop     rcx
-	pop     rax
+    pop     r14
+    pop     r13
+    pop     r12
+    pop     r11
+    pop     r10
+    pop     r9
+    pop     r8
+    pop     rdi
+    pop     rsi
+    pop     rbp
+    pop     rbx
+    pop     rdx
+    pop     rcx
+    pop     rax
 %ENDMACRO
 
 ALIGN       16
@@ -136,8 +136,8 @@ KxHardwareInterrupt:
     call    HalEoi
     add     rsp, 28h
     KiPopTrapFrame
-	add     rsp, 16
-	iretq
+    add     rsp, 16
+    iretq
 
 ALIGN       16
 KxExceptionInterrupt:
